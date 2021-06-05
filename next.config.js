c95aa9865f49214853b74e51b84af394f646d736
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-06-05 18:19:48 +0800

    Remove env from next.config

--- next.config.js
+++ next.config.js
@@ -7,14 +7,4 @@ module.exports = {
     })
     return config
   },
-  env: {
-    DEVELOPMENT_MONGO_URI:
-      'mongodb+srv://rakia:rakiabodyjm4690@cluster0.l89qj.mongodb.net/dvr?retryWrites=true&w=majority',
-    PRODUCTION_MONGO_URI:
-      'mongodb+srv://rakia:rakiabodyjm4690@cluster0.mlcnz.mongodb.net/dvr?retryWrites=true&w=majority',
-    NEXT_PUBLIC_PRODUCTION_URL: 'https://dito.realm1000.com',
-    NEXT_PUBLIC_DEVELOPMENT_URL: 'http://192.168.1.13:4000',
-    GOOGLE_EMAIL: 'rakiabodyjm@gmail.com',
-    GOOGLE_PASSWORD: 'jotrlfelacwmyxao',
-  },
 }
