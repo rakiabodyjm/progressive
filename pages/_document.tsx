e7a05fe2c4283b90676dd0e7bc2a5d90538d4bae
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-06-08 16:37:12 +0800

    Move loading of text in global.css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,7 +29,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-
+          {/* 
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
@@ -37,7 +37,7 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
-          />
+          /> */}
           <link rel="shortcut icon" href="/favicon-32x32.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
