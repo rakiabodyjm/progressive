e77d527470d6b9769dd07b1a1bb67ba29b2ce134
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-07-18 16:56:05 +0800

    Add og:image

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,6 @@ import PromoPackages from '@src/components/pages/Home/PromoPackages'
 import FAQs from '@src/components/pages/Home/FAQs'
 import Contact from '@src/components/pages/Home/Contact'
 import Head from 'next/head'
-
 const useStyles = makeStyles(() => ({
   index: { overflow: 'hidden' },
 }))
@@ -20,6 +19,19 @@ export default function Home() {
         <meta
           name="description"
           content="DITO Sim Packages from REALM1000 are available for bulk ordering and inquiries. Inquire Now to get your special quotation sent directly to your email or phone number"
+        />
+        <meta property="og:title" content="REALM1000 | DITO SIM Packages" />
+        <meta property="og:type" content="website" />
+        {/* <meta property="og:image" content="/og:image.png" /> */}
+        <meta property="og:image" content="/og:image2.png" />
+        <meta
+          property="og:description"
+          content="Inquire and be an official retailer of DITO Telecom Sims, the fastest growing telecom in the country!"
+        />
+        <meta
+          property="og:url"
+          content="https://dito.realm1000.com"
+          // content="Inquire and be an official retailer of DITO Telecom Sims, the fastest growing telecom in the country!"
         />
       </Head>
       <div className={classes.index}>
