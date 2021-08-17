7d0d18df68325be766420003288e521811057a70
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-17 15:03:54 +0800

    Initialize Admin page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,57 @@
 import { CssBaseline } from '@material-ui/core'
 import { ThemeProvider } from '@material-ui/styles'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import '../styles/globals.css'
 import Head from 'next/head'
 import theme from '@src/theme'
 import Nav from '@src/components/layout/Nav/Nav'
-import { Provider } from 'react-redux'
-import store from '@src/redux/store'
+import { Provider, useDispatch, useSelector } from 'react-redux'
+import store, { RootState } from '@src/redux/store'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import { SnackbarProvider, useSnackbar } from 'notistack'
+import Notification from '@src/components/layout/Notification/Notification'
+import jwtDecode from '@src/utils/jwtDecode'
+import { NotificationTypes, setNotification } from '@src/redux/data/notificationSlice'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  const pathname = useMemo(() => router.pathname, [router])
+
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles)
     }
-  })
+  }, [])
+
+  useEffect(() => {
+    axios.defaults.baseURL =
+      process.env.NODE_ENV === 'development'
+        ? process.env.NEXT_PUBLIC_DEVELOPMENT_BACKEND_URL
+        : process.env.NEXT_PUBLIC_PRODUCTION_BACKEND_URL
+  }, [])
+  const inAdminPage = useMemo(() => /admin/.test(pathname), [pathname])
+  useEffect(() => {
+    console.log('pathname', pathname)
+    if (inAdminPage) {
+      console.log('in admin page')
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        const decode = jwtDecode(token)
+        console.log(decode)
+      } else {
+        router.push('/admin/login')
+        store.dispatch(
+          setNotification({
+            message: 'You must log in first',
+            type: NotificationTypes.ERROR,
+          })
+        )
+      }
+    }
+  }, [pathname])
+
   return (
     <>
       <Head>
@@ -26,8 +63,11 @@ function MyApp({ Component, pageProps }) {
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Nav />
-          <Component {...pageProps} />
+          <SnackbarProvider>
+            <Notification />
+            {/admin/.test(pathname) ? null : <Nav />}
+            <Component {...pageProps} />
+          </SnackbarProvider>
         </ThemeProvider>
       </Provider>
     </>
---
+++ pages/admin/index.tsx
@@ -0,0 +1,68 @@
+import { makeStyles, Typography } from '@material-ui/core'
+import DrawerComponent from '@src/components/admin/layout/Drawer'
+import Nav from '@src/components/admin/layout/Nav'
+import useWidth from '@src/utils/useWidth'
+import { useState } from 'react'
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+}))
+export default function AdminIndex() {
+  const width = useWidth()
+  const classes = useStyles()
+  const [open, setOpen] = useState(false)
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+  return (
+    <div
+      style={{
+        display: 'flex',
+      }}
+    >
+      <DrawerComponent handleDrawerClose={handleDrawerClose} open={open} />
+      <Nav open={open} handleDrawerOpen={handleDrawerOpen} />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
+    </div>
+  )
+}
