6816b5379f6acaf1779bac65b63f6094b93874fb
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-14 23:32:30 +0800

    Changes to folder structure page in src

--- pages/dsp/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@material-ui/core'
-
-export default function DspPage() {
-  return (
-    <div>
-      <Typography variant="h4">DSP Management</Typography>
-    </div>
-  )
-}
