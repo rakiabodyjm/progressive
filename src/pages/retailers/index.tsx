9201c8802315992798898b2b6edd16a531c072a8
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-18 23:35:18 +0800

    Individual Pages

---
+++ src/pages/retailers/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@material-ui/core'
+
+export default function DspPage() {
+  return (
+    <div>
+      <Typography variant="h4">Retailer Management</Typography>
+    </div>
+  )
+}
