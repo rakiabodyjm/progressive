6502ede4fe979d375acec0c3e344828bc793c5a5
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-18 23:33:07 +0800

    Update theme for dark mode

--- src/theme.ts
+++ src/theme.ts
@@ -1,91 +1,31 @@
+// import { createTheme, responsiveFontSizes } from '@material-ui/core'
+// import { responsiveFontSizes, createTheme } from '@mui/material/styles'
+
 import { createTheme, responsiveFontSizes } from '@material-ui/core'
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#ed8332',
-      dark: '#ec5b2b',
-      light: '#f3b169',
-      contrastText: '#ffffff',
-    },
-    secondary: {
-      main: '#0F254C',
-      dark: '#000024',
-      light: '#3e4d78',
-      contrastText: '#ffffff',
-    },
-  },
-  typography: {
-    fontFamily: '"Exo 2", Montserrat, Raleway, Roboto, Helvetica, Arial, sans-serif',
-    // fontSize: 14,
 
-    body1: {
-      fontWeight: 500,
-    },
-  },
-  overrides: {
-    MuiCssBaseline: {
-      '@global': {
-        html: {
-          textDecoration: 'none',
-          WebkitFontSmoothing: 'auto',
-          // fontFamily: 'Montserrat, Raleway, Roboto, Helvetica, Arial, sans-serif',
-          fontSize: 14,
-          fontWeight: '600',
-        },
+type CustomThemeProperties = {
+  prefersDarkMode: boolean
+}
+const theme = (additionalProps: CustomThemeProperties) => {
+  const { prefersDarkMode } = additionalProps
+  return createTheme({
+    palette: {
+      type: prefersDarkMode ? 'dark' : 'light',
+      primary: {
+        main: '#ec5b2b',
+      },
+      secondary: {
+        main: '#0F254C',
       },
     },
+    typography: {
+      htmlFontSize: 16,
+      fontSize: 14,
+      fontFamily: [`'Exo 2'`, '-apple-system', 'BlinkMacSystemFont', 'Roboto', 'Oxygen'].join(','),
+    },
+  })
+}
 
-    MuiButton: {
-      root: {
-        borderRadius: 8,
-        borderWidth: '2px',
-      },
-      label: {
-        letterSpacing: '-0.04em',
-      },
-      textSizeLarge: {
-        padding: '8px 22px',
-      },
-    },
+const themeCreator = (params: CustomThemeProperties) => responsiveFontSizes(theme(params))
 
-    MuiInputBase: {
-      input: {
-        // height: '0.7em',
-        fontWeight: 600,
-      },
-    },
-    MuiFilledInput: {
-      inputMarginDense: {
-        paddingTop: '14px',
-      },
-    },
-
-    MuiTypography: {
-      root: {
-        // letterSpacing: '-0.06em',
-      },
-      h1: {
-        lineHeight: 1,
-      },
-    },
-
-    MuiPaper: {
-      rounded: {
-        borderRadius: 8,
-      },
-    },
-
-    MuiDrawer: {
-      paper: {
-        borderRadius: `0 0 8px 8px`,
-      },
-    },
-    MuiSelect: {
-      root: {
-        padding: `8px`,
-      },
-    },
-  },
-})
-
-export default responsiveFontSizes(theme)
+export default themeCreator
