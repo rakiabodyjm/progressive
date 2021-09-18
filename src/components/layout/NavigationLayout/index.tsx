3aa12d12d1d9bc543f13e4391c117ddbce4df08f
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-18 23:14:29 +0800

    Remove routing logic from NavigationLayout component

--- src/components/layout/NavigationLayout/index.tsx
+++ src/components/layout/NavigationLayout/index.tsx
@@ -1,4 +1,5 @@
 import { makeStyles, Typography } from '@material-ui/core'
+import { useTheme } from '@material-ui/styles'
 import Drawer from '@src/components/layout/NavigationLayout/Drawer'
 import Nav from '@src/components/layout/NavigationLayout/Nav'
 import LoadingScreen from '@src/components/screens/LoadingScreen'
@@ -6,14 +7,15 @@ import { getUser, User, UserState } from '@src/redux/data/userSlice'
 import { RootState } from '@src/redux/store'
 import useWidth from '@src/utils/hooks/useWidth'
 import { useRouter } from 'next/router'
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const useStyles = makeStyles((theme) => ({
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
+    minHeight: '90vh',
   },
   toolbar: {
     display: 'flex',
@@ -24,6 +26,7 @@ const useStyles = makeStyles((theme) => ({
     ...theme.mixins.toolbar,
   },
 }))
+
 export default function NavigationLayout({ children }) {
   const width = useWidth()
   const classes = useStyles()
@@ -31,32 +34,12 @@ export default function NavigationLayout({ children }) {
   const handleDrawerOpen = () => {
     setOpen(true)
   }
-  const user: User = useSelector((state: RootState) => state.user?.data)
   const handleDrawerClose = () => {
     setOpen(false)
   }
-  const router = useRouter()
-  const dispatch = useDispatch()
+  const userSelect = useSelector((state: RootState) => state.user)
+  const user = useMemo(() => userSelect?.data || null, [userSelect])
 
-  useEffect(() => {
-    if (!user) {
-      router.push('/')
-    }
-    /**
-     * if user exists
-     */
-    if (user) {
-      dispatch(getUser(user.user_id))
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  if (!user) {
-    return <>{children}</>
-  }
-  if (!user && !children) {
-    return <LoadingScreen textColor="var(--secondary-main)" color="var(--primary-dark)" />
-  }
   return (
     <div
       style={{
@@ -64,8 +47,8 @@ export default function NavigationLayout({ children }) {
       }}
     >
       <Drawer
-        userName={user.first_name}
-        roles={user.roles}
+        userName={user?.first_name || 'Loading...'}
+        roles={user?.roles || []}
         handleDrawerClose={handleDrawerClose}
         open={open}
       />
