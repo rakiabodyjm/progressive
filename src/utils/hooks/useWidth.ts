0a0d58b6a87f3570d1ef385223932495ba31b7d5
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-21 10:08:35 +0800

    useWidth updated types

--- src/utils/hooks/useWidth.ts
+++ src/utils/hooks/useWidth.ts
@@ -1,16 +1,23 @@
 import { useTheme, useMediaQuery } from '@material-ui/core'
+import { Breakpoint } from '@material-ui/core/styles/createBreakpoints'
+import { useCallback, useMemo } from 'react'
 
-export const useWidth = (): 'xs' | 'sm' | 'md' | 'lg' | 'xl' => {
+export const useWidth = (): Breakpoint => {
   const theme = useTheme()
-  const keys = [...theme.breakpoints.keys].reverse()
-
-  return (
-    keys.reduce((output, key) => {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const matches = useMediaQuery(theme.breakpoints.up(key))
-      return !output && matches ? key : output
-    }, null) || 'xs'
+  const keys: Breakpoint[] = [...theme.breakpoints.keys].reverse()
+  const mediaQueryFn = useMediaQuery
+  const matches = useCallback(
+    (key) => {
+      mediaQueryFn(theme.breakpoints.up(key))
+    },
+    [mediaQueryFn, theme.breakpoints]
   )
+  return keys.reduce((output: Breakpoint, key) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const matches = mediaQueryFn(theme.breakpoints.up(key))
+    // const matches = useMediaQuery(theme.breakpoints.up(key))
+    return !output && matches ? key : output
+  }, 'xs')
 }
 interface IsMobileTypes {
   tabletIncluded?: boolean
