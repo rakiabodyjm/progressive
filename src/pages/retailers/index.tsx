ccca8f9325725dbf82ca8fff8ecfa36ed51eb630
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-24 10:13:33 +0800

    Added Retailers index page

--- src/pages/retailers/index.tsx
+++ src/pages/retailers/index.tsx
@@ -1,6 +1,6 @@
 import { Typography } from '@material-ui/core'
 
-export default function DspPage() {
+export default function RetailersPage() {
   return (
     <div>
       <Typography variant="h4">Retailer Management</Typography>
