192f77275685cf013f624d636d5964f9911b8df6
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-24 10:13:06 +0800

    Added Subdistributor index page

---
+++ src/pages/subdistributors/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@material-ui/core'
+
+export default function SubdistributorsPage() {
+  return (
+    <div>
+      <Typography variant="h4">Subdistributor Management</Typography>
+    </div>
+  )
+}
