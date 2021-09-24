e22a5638f054c52697396af7157f634d28aeea10
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-24 10:13:55 +0800

    Added Admin Users Management index page

---
+++ src/pages/admin/users/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@material-ui/core'
+
+export default function AdminUsersPage() {
+  return (
+    <div>
+      <Typography variant="h4">User Management</Typography>
+    </div>
+  )
+}
