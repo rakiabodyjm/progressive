c935578443b1e2cb3cdee3cf9ef1ef721f9dd708
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-25 10:16:44 +0800

    Added functions in userApi (keyIntoReadables and errorExtraction from backend)

--- src/utils/api/userApi.ts
+++ src/utils/api/userApi.ts
@@ -1,17 +1,12 @@
 // import type { User, UserMetaData } from '@src/redux/data/userSlice'
-import type { User, UserMetaData, UserState } from '@src/redux/data/userSlice'
+import type { User, UserMetaData, UserState, UserTypes } from '@src/redux/data/userSlice'
 import { AdminResponseType } from '@src/utils/api/adminApi'
 import { DspResponseType } from '@src/utils/api/dspApi'
 import { RetailerResponseType } from '@src/utils/api/retailerApi'
+import { SubdistributorResponseType } from '@src/utils/api/subdistributorApi'
 import jwtDecode from '@src/utils/lib/jwtDecode'
 import axios, { AxiosError, AxiosResponse } from 'axios'
 
-export enum UserRoles {
-  ADMIN = 'admin',
-  DSP = 'dsp',
-  RETAILER = 'retailer',
-  SUBDISTRIBUTOR = 'subdistributor',
-}
 export type LoginUserParams = {
   email: string
   password: string
@@ -35,7 +30,8 @@ export type UserResponse = {
   dsp?: DspResponseType
   admin?: AdminResponseType
   retailer?: RetailerResponseType
-  roles?: UserRoles[]
+  subdistributor?: SubdistributorResponseType
+  roles: UserTypes[]
 }
 export default {
   /**
@@ -57,15 +53,23 @@ export default {
         window?.localStorage?.setItem('token', res.data.access_token)
         return decoded
       })
-      .catch((err) => {
+      .catch((err: AxiosError) => {
         throw err
       })
   },
-
+  /**
+   * Logs out user and removes token from localStorage
+   */
   logoutUser() {
     window?.localStorage?.removeItem('token')
   },
-  getUser(id: string, params?: { cached: boolean }) {
+  /**
+   *
+   * Gets specific user data from parameter returning UserResponse
+   * Second parameter if it's fresh data from db
+   * @returns {UserResponse}
+   */
+  getUser(id: string, params?: { cached: boolean }): Promise<UserResponse> {
     return axios
       .get(`/user/${id}?cache=${!!params?.cached || false}`)
       .then(({ data }: { data: UserResponse }) => data)
@@ -73,15 +77,61 @@ export default {
         throw err
       })
   },
-
-  reduceUser(response: UserResponse): UserState['data'] {
-    const { id, email, first_name, last_name, roles } = response
-    return {
-      user_id: id,
-      email,
-      first_name,
-      last_name,
-      roles,
+  /**
+   * Creates Keys from 'user_api' to 'User Api'
+   * id => "User ID"
+   * roles => "Account Type"
+   * @param param
+   * @returns {string}
+   */
+  formatKeyIntoReadables(param: string) {
+    if (typeof param !== 'string') {
+      return JSON.stringify(param)
+      // throw new Error(`To Readable case received non string ${JSON.stringify(param)}`)
     }
+
+    const toCapsFirst = (toCapitalParam: string): string =>
+      toCapitalParam
+        .split('_')
+        .map((ea) => ea.charAt(0).toUpperCase() + ea.slice(1))
+        .join(' ')
+
+    if (param.indexOf('_') > 0) {
+      return toCapsFirst(param)
+    }
+    if (param === 'id') {
+      return 'User ID'
+    }
+    if (param === 'roles') {
+      return 'Account Types'
+    }
+
+    return toCapsFirst(param)
   },
+  updateUser(id: string, args: Partial<UserResponse>) {
+    return axios
+      .patch(`/user/${id}`, args)
+      .then((res) => res)
+      .catch((err: AxiosError) => {
+        throw err
+      })
+  },
+  extractError(err: AxiosError): string {
+    const errResponse: string | string[] = err?.response?.data?.message
+    const errRequest = err?.request
+    if (errResponse) {
+      if (Array.isArray(errResponse)) {
+        return errResponse.join(', ')
+      }
+      return errResponse
+    }
+    if (errRequest) {
+      return errRequest
+    }
+    return err.message
+  },
+  /**
+   *
+   * reduceUser used by userSlice to convert userdata fetched into something more useful
+   */
 }
