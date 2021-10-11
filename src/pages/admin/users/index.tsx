065fd7858cbb9f3222e52e8f7e499615aa6f5838
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-11 12:54:33 +0800

    Admin's View of all accounts

--- src/pages/admin/users/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@material-ui/core'
-
-export default function AdminUsersPage() {
-  return (
-    <div>
-      <Typography variant="h4">User Management</Typography>
-    </div>
-  )
-}
