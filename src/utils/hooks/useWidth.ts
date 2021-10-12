e653cdf34db622eeb68fe839ab4d5e676702f829
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-12 13:00:49 +0800

    Fix useWidth hook

--- src/utils/hooks/useWidth.ts
+++ src/utils/hooks/useWidth.ts
@@ -1,24 +1,21 @@
 import { useTheme, useMediaQuery } from '@material-ui/core'
 import { Breakpoint } from '@material-ui/core/styles/createBreakpoints'
-import { useCallback, useMemo } from 'react'
+import { useCallback } from 'react'
 
 export const useWidth = (): Breakpoint => {
   const theme = useTheme()
-  const keys: Breakpoint[] = [...theme.breakpoints.keys].reverse()
-  const mediaQueryFn = useMediaQuery
-  const matches = useCallback(
-    (key) => {
-      mediaQueryFn(theme.breakpoints.up(key))
-    },
-    [mediaQueryFn, theme.breakpoints]
-  )
+  const keys = [...theme.breakpoints.keys]
+
   return keys.reduce((output: Breakpoint, key) => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const matches = mediaQueryFn(theme.breakpoints.up(key))
-    // const matches = useMediaQuery(theme.breakpoints.up(key))
-    return !output && matches ? key : output
+    const match = useMediaQuery(theme.breakpoints.up(key))
+    if (match) {
+      return key
+    }
+    return output
   }, 'xs')
 }
+
 interface IsMobileTypes {
   tabletIncluded?: boolean
   tabletOnly?: boolean
