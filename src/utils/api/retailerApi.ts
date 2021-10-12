9f59fb0be3a499f2aca0a666103353d6157127cf
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-12 13:05:09 +0800

    retailerApi types and functions

--- src/utils/api/retailerApi.ts
+++ src/utils/api/retailerApi.ts
@@ -1,5 +1,25 @@
+import { extractErrorFromResponse } from '@src/utils/api/common'
+import { DspResponseType } from '@src/utils/api/dspApi'
+import { SubdistributorResponseType } from '@src/utils/api/subdistributorApi'
 import type { UserResponse } from '@src/utils/api/userApi'
+import axios, { AxiosError } from 'axios'
 
+// Update MapId 09-28 11:03
 export type RetailerResponseType = {
-  user?: UserResponse
+  id: string
+  e_bind_number: string
+  store_name: string
+  id_type: string
+  id_number: string
+  subdistributor: SubdistributorResponseType
+  dsp: DspResponseType
+  user: UserResponse
 }
+
+export const getRetailer = (id: string): Promise<RetailerResponseType> =>
+  axios
+    .get(`/retailer/${id}`)
+    .then((res) => res.data)
+    .catch((err: AxiosError) => {
+      throw new Error(extractErrorFromResponse(err))
+    })
