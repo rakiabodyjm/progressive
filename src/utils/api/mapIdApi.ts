0450dd09b9f04b53965aea1abf6f000f781a7ecc
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-12 13:05:18 +0800

    mapidApi types and functions

---
+++ src/utils/api/mapIdApi.ts
@@ -0,0 +1,43 @@
+import { DspResponseType } from '@src/utils/api/dspApi'
+import { SubdistributorResponseType } from '@src/utils/api/subdistributorApi'
+import axios from 'axios'
+
+// Update MapId 09-28 11:03
+export interface MapIdResponseType {
+  area_id: string
+  area_name: string
+  parent_name: string
+  parent_parent_name: string
+  area_parent_pp_name: string
+  // dsp?: DspResponseType[]
+  // subdistributors?: SubdistributorResponseType[]
+}
+
+interface SearchMap {
+  search?: string | undefined
+  page?: number
+  limit?: number
+}
+export function searchMap(params?: SearchMap) {
+  return axios
+    .get('/map-ids', {
+      params: {
+        search: params?.search || undefined,
+        page: params?.page || 0,
+        limit: params?.limit || 100,
+      },
+    })
+    .then((res) => res.data as MapIdResponseType)
+    .catch((err) => {
+      throw err
+    })
+}
+
+export function getMap(id: string) {
+  return axios
+    .get(`/map-ids/${id}`)
+    .then((res) => res.data)
+    .catch((err) => {
+      throw err
+    })
+}
