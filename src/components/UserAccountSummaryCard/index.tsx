0dde951ca5ca9c1c9e9a0bc49292f527acfb48f4
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-13 14:33:45 +0800

    Restructure all components into one folder

---
+++ src/components/UserAccountSummaryCard/index.tsx
@@ -0,0 +1,60 @@
+import { Paper, Box, Typography } from '@material-ui/core'
+import { UserResponse } from '@src/utils/api/userApi'
+
+export default function UserAccountSummaryCard({ account }: { account: UserResponse | undefined }) {
+  return (
+    <Paper
+      style={{
+        height: '100%',
+      }}
+      variant="outlined"
+    >
+      <Box p={2}>
+        <Typography
+          style={{
+            marginBottom: 16,
+          }}
+          variant="h5"
+        >
+          User Account Summary
+        </Typography>
+        {account &&
+          userAccountFields(account).map(({ key, value }) => (
+            <Box mb={1} key={key}>
+              <Typography color="primary" variant="body2">
+                {key}:
+              </Typography>
+              <Typography variant="body1">{value}</Typography>
+            </Box>
+          ))}
+      </Box>
+    </Paper>
+  )
+}
+
+const userAccountFields = (params: UserResponse) => [
+  {
+    key: 'Name',
+    value: `${params.first_name} ${params.last_name}`,
+  },
+  {
+    key: 'Contact Number',
+    value: params.phone_number,
+  },
+  {
+    key: 'Email Address',
+    value: params.email,
+  },
+  {
+    key: 'Username',
+    value: params.username,
+  },
+  {
+    key: 'Primary Address',
+    value: params.address1,
+  },
+  {
+    key: 'Secondary Address',
+    value: params?.address2 || null,
+  },
+]
