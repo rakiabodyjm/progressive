9cc9c18ca5ccdac05b1145d2fc2abdbb6d35d465
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-14 01:25:33 +0800

    View user account component

--- src/components/pages/user/ViewUserAccount/index.tsx
+++ src/components/pages/user/ViewUserAccount/index.tsx
@@ -1,25 +1,31 @@
-import { Box, Divider, Paper, PaperProps, Theme, Typography } from '@material-ui/core'
+import { Box, Divider, Grid, Paper, PaperProps, Theme, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
-import ObjectRenderer from '@src/components/ObjectRenderer'
+import AestheticObjectRenderer from '@src/components/ObjectRendererV2'
+import UserAccountSummaryCard from '@src/components/UserAccountSummaryCard'
 import { UserResponse } from '@src/utils/api/userApi'
+import { useEffect } from 'react'
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     padding: theme.spacing(2),
   },
 }))
-export default function ViewUserAccount({ user, ...restProps }: { user: Partial<UserResponse> }) {
+export default function ViewUserAccount({ user, ...restProps }: { user: UserResponse }) {
   const classes = useStyles()
+
   return (
     <Paper {...restProps} className={classes.root}>
       <Typography
         style={{
           fontWeight: 600,
         }}
-        color="primary"
+        // color="primary"
         variant="h4"
       >
-        User Account
+        User Account Information
+      </Typography>
+      <Typography color="primary" variant="body1">
+        View or Edit User Account information
       </Typography>
       <Divider
         style={{
@@ -27,7 +33,29 @@ export default function ViewUserAccount({ user, ...restProps }: { user: Partial<
           marginBottom: 24,
         }}
       />
-      <ObjectRenderer<Partial<UserResponse>> fields={user} />
+      <Grid spacing={2} container>
+        <Grid item xs={12} md={6}>
+          {/* <UserAccountSummaryCard account={user} /> */}
+          <Paper variant="outlined">
+            <Box p={2}>
+              <AestheticObjectRenderer
+                // renderObject
+                highlight="key"
+                spacing={1}
+                fields={formatUsers(user)}
+              />
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
     </Paper>
   )
 }
+
+/**
+ * Just in case we need to format displaying UserResponse
+ * @param param
+ */
+const formatUsers = (param: UserResponse) => ({
+  ...param,
+})
