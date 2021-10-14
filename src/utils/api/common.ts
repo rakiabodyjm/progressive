bd7bf598b36555c018fc765537475182f96482bc
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-15 01:28:45 +0800

    Add formatKeyintoreadables into common utilities

--- src/utils/api/common.ts
+++ src/utils/api/common.ts
@@ -15,3 +15,35 @@ export const extractErrorFromResponse = (err: AxiosError) => {
   }
   return err.message
 }
+
+export function formatKeyIntoReadables(param: string) {
+  if (typeof param !== 'string') {
+    return JSON.stringify(param)
+    // throw new Error(`To Readable case received non string ${JSON.stringify(param)}`)
+  }
+
+  const toCapsFirst = (toCapitalParam: string): string =>
+    toCapitalParam
+      .split('_')
+      .map((ea) => ea.charAt(0).toUpperCase() + ea.slice(1))
+      .join(' ')
+
+  if (param.indexOf('_') > 0) {
+    return toCapsFirst(param)
+  }
+  if (param === 'id') {
+    return 'User ID'
+  }
+  if (param === 'roles') {
+    return 'Account Types'
+  }
+
+  switch (param) {
+    case 'address1':
+      return 'Address 1'
+    case 'address2':
+      return 'Address 2'
+    default:
+      return toCapsFirst(param)
+  }
+}
