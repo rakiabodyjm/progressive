2da925a51ab6557ad30c5f22d7d876863810cd6d
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-19 10:06:13 +0800

    View SubdistributorAccountPage

--- src/components/pages/subdistributor/ViewSubdistributorAccount/index.tsx
+++ src/components/pages/subdistributor/ViewSubdistributorAccount/index.tsx
@@ -18,7 +18,6 @@ import {
   getSubdistributor,
   SubdistributorResponseType,
 } from '@src/utils/api/subdistributorApi'
-import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import useSWR from 'swr'
 
@@ -33,87 +32,86 @@ export default function ViewSubdistributorAccount({
 }: {
   subdistributorId: string
 } & PaperProps) {
-  const [subdistributor, setSubdistributor] = useState<SubdistributorResponseType>()
+  // const [subdistributor, setSubdistributor] = useState<SubdistributorResponseType>()
   const classes = useStyles()
   const theme: Theme = useTheme()
   const dispatch = useDispatch()
+  const { data: subdistributor, error } = useSWR(subdistributorId, (id) =>
+    getSubdistributor(id)
+      .then((res) => res)
+      .catch((err) => {
+        dispatch(
+          setNotification({
+            type: NotificationTypes.ERROR,
+            message: err.message,
+          })
+        )
+        console.error(err)
+      })
+  )
 
-  useEffect(() => {
-    if (subdistributorId) {
-      getSubdistributor(subdistributorId)
-        .then((res) => {
-          setSubdistributor(res)
-        })
-        .catch((err) => {
-          dispatch(
-            setNotification({
-              type: NotificationTypes.ERROR,
-              message: err.message,
-            })
-          )
-        })
-    }
-  }, [subdistributorId, dispatch])
   return (
-    <Paper className={classes.root} {...restProps} variant="outlined">
-      <Typography variant="h4">Subdistributor Account Management</Typography>
-      <Typography color="primary" variant="subtitle2">
-        Manage Sudbistributor Account
-      </Typography>
+    <>
+      <Paper className={classes.root} {...restProps} variant="outlined">
+        <Typography variant="h4">Subdistributor Account Management</Typography>
+        <Typography color="primary" variant="subtitle2">
+          Manage Sudbistributor Account
+        </Typography>
 
-      <Divider
-        style={{
-          margin: `16px 0`,
-          marginBottom: 24,
-        }}
-      />
-      {subdistributor ? (
-        <Box>
-          <Grid spacing={2} container>
-            <Grid xs={12} md={6} item>
-              <SubdistributorAccountSummaryCard subdistributor={subdistributor} />
-            </Grid>
-            <Grid xs={12} md={6} item></Grid>
-            <Grid xs={12} item>
-              <Box mt={2}>
-                <Paper variant="outlined">
-                  <Box p={2}>
-                    <Typography variant="h5">DSP Accounts</Typography>
-                    <Typography variant="subtitle2" color="primary">
-                      DSP Accounts this Subdistributor owns
-                    </Typography>
-                  </Box>
-
-                  <DspTable subdistributorId={subdistributor.id} />
-                </Paper>
-              </Box>
-            </Grid>
-            <Grid xs={12} item>
-              <Box mt={2}>
-                <Paper variant="outlined">
-                  <Box p={2}>
-                    <Typography variant="h5">Retailer Accounts</Typography>
-                    <Typography variant="subtitle2" color="primary">
-                      Retailer Accounts this Subdistributor Services
-                    </Typography>
-                  </Box>
-
-                  <RetailerTable subdistributorId={subdistributor.id} />
-                </Paper>
-              </Box>
-            </Grid>
-          </Grid>
-        </Box>
-      ) : (
-        <Box
+        <Divider
           style={{
-            textAlign: 'center',
-            ...restProps.style,
+            margin: `16px 0`,
+            marginBottom: 24,
           }}
-        >
-          <CircularProgress size={theme.typography.h1.fontSize} thickness={4} color="primary" />
-        </Box>
-      )}
-    </Paper>
+        />
+        {subdistributor ? (
+          <Box>
+            <Grid spacing={2} container>
+              <Grid xs={12} md={6} item>
+                <SubdistributorAccountSummaryCard subdistributor={subdistributor} />
+              </Grid>
+              <Grid xs={12} md={6} item></Grid>
+              <Grid xs={12} item>
+                <Box mt={2}>
+                  <Paper variant="outlined">
+                    <Box p={2}>
+                      <Typography variant="h5">DSP Accounts</Typography>
+                      <Typography variant="subtitle2" color="primary">
+                        DSP Accounts this Subdistributor owns
+                      </Typography>
+                    </Box>
+
+                    <DspTable subdistributorId={subdistributor.id} />
+                  </Paper>
+                </Box>
+              </Grid>
+              <Grid xs={12} item>
+                <Box mt={2}>
+                  <Paper variant="outlined">
+                    <Box p={2}>
+                      <Typography variant="h5">Retailer Accounts</Typography>
+                      <Typography variant="subtitle2" color="primary">
+                        Retailer Accounts this Subdistributor Services
+                      </Typography>
+                    </Box>
+
+                    <RetailerTable subdistributorId={subdistributor.id} />
+                  </Paper>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+        ) : (
+          <Box
+            style={{
+              textAlign: 'center',
+              ...restProps.style,
+            }}
+          >
+            <CircularProgress size={theme.typography.h1.fontSize} thickness={4} color="primary" />
+          </Box>
+        )}
+      </Paper>
+    </>
   )
 }
