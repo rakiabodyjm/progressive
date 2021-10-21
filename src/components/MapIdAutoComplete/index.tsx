ef8e5e9c0c185a0be670f2cbf9dc4a8fca7d56b5
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-21 10:10:37 +0800

    Create Single MapID Autocomplete

---
+++ src/components/MapIdAutoComplete/index.tsx
@@ -0,0 +1,24 @@
+import SimpleAutoComplete from '@src/components/SimpleAutoComplete'
+import { MapIdResponseType, SearchMap, searchMap } from '@src/utils/api/mapIdApi'
+
+export default function MapIdAutoComplete({
+  onChange,
+  mutateOptions,
+}: {
+  onChange: (arg: MapIdResponseType) => void
+  mutateOptions?: (arg: MapIdResponseType[]) => MapIdResponseType[]
+}) {
+  return (
+    <SimpleAutoComplete<MapIdResponseType, SearchMap>
+      initialQuery={{ limit: 100, page: 0, search: '' }}
+      querySetter={(prevState, inputValue) => ({
+        ...prevState,
+        search: inputValue,
+      })}
+      fetcher={(q) => searchMap(q as SearchMap)}
+      onChange={onChange}
+      getOptionLabel={(option: MapIdResponseType) => `${option.area_name}, ${option.parent_name}`}
+      getOptionSelected={(current, selected) => current.area_id === selected.area_id}
+    />
+  )
+}
