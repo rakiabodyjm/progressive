8f74364dc514b7257000ad02f29b7309abf77e5a
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-27 15:19:31 +0800

    CreateRetailerAccount Form

--- src/components/pages/retailer/CreateRetailerAccount/index.tsx
+++ src/components/pages/retailer/CreateRetailerAccount/index.tsx
@@ -1,7 +1,6 @@
 import {
   Box,
   Button,
-  CircularProgress,
   Divider,
   Grid,
   IconButton,
@@ -16,17 +15,26 @@ import { Autocomplete } from '@material-ui/lab'
 import SimpleAutoComplete from '@src/components/SimpleAutoComplete'
 import UserAutoComplete from '@src/components/UserAutoComplete'
 import { NotificationTypes } from '@src/redux/data/notificationSlice'
-import { DspResponseType } from '@src/utils/api/dspApi'
+import { DspResponseType, getDsp } from '@src/utils/api/dspApi'
 import { CreateRetailer, createRetailer } from '@src/utils/api/retailerApi'
 import {
   getDsps,
+  getSubdistributor,
   searchSubdistributor,
   SubdistributorResponseType,
 } from '@src/utils/api/subdistributorApi'
 import useNotification from '@src/utils/hooks/useNotification'
 import React, { ChangeEvent, useEffect, useState } from 'react'
 
-export default function CreateRetailerAccount({ modal: modalClose }: { modal?: () => void }) {
+export default function CreateRetailerAccount({
+  modal: modalClose,
+  subdistributorId,
+  dspId,
+}: {
+  modal?: () => void
+  subdistributorId?: string
+  dspId?: string
+}) {
   const [newRetailerAccount, setNewRetailerAccount] = useState<
     Record<keyof CreateRetailer, string | null>
   >({
@@ -38,6 +46,22 @@ export default function CreateRetailerAccount({ modal: modalClose }: { modal?: (
     subdistributor: null,
     dsp: null,
   })
+
+  const { entity: autoLoadDsp, loading: autoLoadDspLoading } = useFetchEntity('dsp', dspId)
+
+  const { entity: autoLoadSubdistributor, loading: autoLoadSubdistributorLoading } = useFetchEntity(
+    'subdistributor',
+    subdistributorId
+  )
+  useEffect(() => {
+    if (autoLoadSubdistributor) {
+      handleChange('subdistributor', autoLoadSubdistributor?.id)
+    }
+    if (autoLoadDsp) {
+      handleChange('dsp', autoLoadDsp?.id)
+    }
+    // console.log(autoLoadSubdistributor, autoLoadDsp)
+  }, [autoLoadSubdistributor, autoLoadDsp])
 
   const handleChange = (e: unknown | ChangeEvent<HTMLInputElement>, value?: string | null) => {
     if (typeof e !== 'string') {
@@ -55,9 +79,12 @@ export default function CreateRetailerAccount({ modal: modalClose }: { modal?: (
   }
   const { subdistributor } = newRetailerAccount
 
-  const [dspOptions, setDspOptions] = useState<DspResponseType[]>([])
+  const [dspOptions, setDspOptions] = useState<DspResponseType[] | undefined>()
   useEffect(() => {
-    if (subdistributor) {
+    /**
+     * Fetch only if there is not dspId given
+     */
+    if (subdistributor && !dspId) {
       getDsps(subdistributor)
         .then((res) => {
           setDspOptions(res.data)
@@ -66,7 +93,11 @@ export default function CreateRetailerAccount({ modal: modalClose }: { modal?: (
           console.error(err)
         })
     }
-  }, [subdistributor])
+    if (dspId && autoLoadDsp) {
+      setDspOptions([autoLoadDsp as DspResponseType])
+    }
+  }, [subdistributor, dspId, autoLoadDsp])
+
   const dispatchNotif = useNotification()
 
   const handleSubmit = () => {
@@ -172,16 +203,22 @@ export default function CreateRetailerAccount({ modal: modalClose }: { modal?: (
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <TypographyLabel>Subdistributor</TypographyLabel>
-            <SimpleAutoComplete<SubdistributorResponseType, string>
-              initialQuery=""
-              fetcher={(q) => searchSubdistributor(q || ' ')}
-              getOptionLabel={(option) => option.name}
-              getOptionSelected={(val1, val2) => val1.id === val2.id}
-              querySetter={(arg, inputValue) => inputValue}
-              onChange={(value) => {
-                handleChange('subdistributor', value?.id || null)
-              }}
-            />
+            {(subdistributorId && autoLoadSubdistributor) || !subdistributorId ? (
+              <SimpleAutoComplete<SubdistributorResponseType, string>
+                initialQuery=""
+                fetcher={(q) => searchSubdistributor(q || ' ')}
+                getOptionLabel={(option) => option.name}
+                getOptionSelected={(val1, val2) => val1.id === val2.id}
+                querySetter={(arg, inputValue) => inputValue}
+                onChange={(value) => {
+                  handleChange('subdistributor', value?.id || null)
+                }}
+                defaultValue={autoLoadSubdistributor as SubdistributorResponseType}
+                disabled={subdistributorId ? true : undefined}
+              />
+            ) : (
+              <CustomTextField name="subdistributor" disabled />
+            )}
           </Grid>
         </Grid>
         <Grid container spacing={2}>
@@ -206,6 +243,8 @@ export default function CreateRetailerAccount({ modal: modalClose }: { modal?: (
                     }}
                   />
                 )}
+                defaultValue={(autoLoadDsp as DspResponseType) || null}
+                disabled={autoLoadDsp ? true : undefined}
               />
             ) : (
               <CustomTextField
@@ -267,6 +306,41 @@ const TypographyLabel = ({
 // const CustomTextField = ({ name, ...restProps }: { name: string } & TextFieldProps) => (
 
 // )
+const useFetchEntity = (type: 'dsp' | 'subdistributor', id?: string) => {
+  const [entity, setEntity] = useState<DspResponseType | SubdistributorResponseType>()
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const fetcher = (argType: typeof type) => {
+    if (argType === 'dsp') {
+      return getDsp
+    }
+    if (argType === 'subdistributor') {
+      return getSubdistributor
+    }
+
+    throw new Error('useFetch entity must have type DSP | Subdistributor')
+  }
+  useEffect(() => {
+    if (type && id) {
+      setLoading(true)
+      fetcher(type)(id)
+        .then((res) => {
+          setEntity(res)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    }
+  }, [type, id])
+
+  return {
+    loading,
+    entity,
+  }
+}
 
 function CustomTextField<T extends CreateRetailer>({
   name,
