f831e08d1593f950f71eda61c28f81ea48e25306
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-11-29 15:17:15 +0800

    inventoryApi

---
+++ src/utils/api/inventoryApi.ts
@@ -0,0 +1,68 @@
+/* eslint-disable no-redeclare */
+import { Asset } from '@src/utils/api/assetApi'
+import { extractErrorFromResponse, extractMultipleErrorFromResponse } from '@src/utils/api/common'
+import { CeasarWalletResponse } from '@src/utils/api/walletApi'
+import { Paginated, PaginateFetchParameters } from '@src/utils/types/PaginatedEntity'
+import axios from 'axios'
+import { EntityWithMessage } from '../types/EntityMessage'
+
+export interface Inventory {
+  id: string
+  quantity: number
+  asset: Asset
+  ceasar: CeasarWalletResponse
+}
+
+export interface CreateInventory {
+  quantity: number
+  asset: string
+  // ceasar: string
+}
+
+export type UpdateInventory = {
+  quantity: number
+}
+
+export function getAllInventory(params: PaginateFetchParameters): Promise<Paginated<Inventory>>
+export function getAllInventory(...params: never): Promise<Inventory[]>
+export function getAllInventory(params: unknown) {
+  return axios
+    .get('/inventory', {
+      params,
+    })
+    .then((res) => res.data)
+    .catch((err) => {
+      throw extractErrorFromResponse(err)
+    })
+}
+
+export function getInventory(id: Inventory['id']): Promise<Inventory> {
+  return axios
+    .get(`/inventory/${id}`)
+    .then((res) => res.data)
+    .catch((err) => {
+      throw extractErrorFromResponse(err)
+    })
+}
+
+export function adminAcquireInventory(params: CreateInventory) {
+  return axios
+    .post('/inventory/admin-acquire', {
+      ...params,
+    })
+    .then((res) => res.data as Inventory)
+    .catch((err) => {
+      throw extractMultipleErrorFromResponse(err)
+    })
+}
+
+export function updateInventory(id: Inventory['id'], updateInventoryDto: UpdateInventory) {
+  return axios
+    .patch(`/inventory/${id}`, {
+      ...updateInventoryDto,
+    })
+    .then((res) => res.data as EntityWithMessage<Inventory>)
+    .catch((err) => {
+      throw extractMultipleErrorFromResponse(err)
+    })
+}
