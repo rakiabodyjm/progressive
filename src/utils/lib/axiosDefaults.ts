c77f64d501716c73489febd02397589ad55c331a
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-12-11 11:42:13 +0800

    ENV Fix for one NEXT_PUBLIC_BACKEND_URL instead

--- src/utils/lib/axiosDefaults.ts
+++ src/utils/lib/axiosDefaults.ts
@@ -4,10 +4,8 @@ import store from '@src/redux/store'
 import axios, { AxiosResponse } from 'axios'
 
 export default function axiosDefaults() {
-  axios.defaults.baseURL =
-    process.env.NODE_ENV === 'development'
-      ? process.env.NEXT_PUBLIC_DEVELOPMENT_BACKEND_URL
-      : process.env.NEXT_PUBLIC_PRODUCTION_BACKEND_URL
+  axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_URL
+
   /**
    * have interceptor
    */
