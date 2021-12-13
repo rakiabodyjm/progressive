1154a7681120ab0eb30ef2176e0a641f6f0380c7
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-12-13 14:15:59 +0800

    Add nextjs customServer, create terminal output for en

---
+++ server.js
@@ -0,0 +1,50 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+const chalk = require('chalk')
+const cliBoxes = require('cli-boxes')
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const port = process.env.PORT || 2006
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    handle(req, res, parsedUrl)
+  }).listen(port, (err) => {
+    if (err) throw err
+    /**
+     * Initialize borders
+     */
+
+    const topLeft = `${chalk.blueBright(cliBoxes.double.topLeft)}`
+    const topRight = `${chalk.blueBright(cliBoxes.double.topRight)}`
+    const line = `${chalk.blueBright(cliBoxes.double.top).repeat(process.stdout.columns - 2)}`
+    const topBorder = `${topLeft}${line}${topRight}`
+    const bottomBorder = `${chalk.blueBright(cliBoxes.double.bottomLeft)}${line}${chalk.blueBright(
+      cliBoxes.double.bottomRight
+    )}`
+
+    /**
+     * Output
+     */
+
+    console.log(`
+${topBorder}
+
+${chalk.bgRedBright('REALM1000 TELCO PROJECT')}
+
+
+${`${chalk.greenBright('Ready on PORT: ')}${chalk.yellow(port)}`}
+
+
+${chalk.greenBright('BACKEND_HOSTNAME: ') + chalk.yellow(process.env.BACKEND_HOSTNAME)}
+
+${bottomBorder}
+`)
+  })
+})
