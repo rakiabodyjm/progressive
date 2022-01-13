624fb4b9ac8f2004cf790ec2fd50a4d7d1c75af9
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-01-13 18:03:25 +0800

    ECommerce Panel

--- src/components/ECommerce/index.tsx
+++ src/components/ECommerce/index.tsx
@@ -2,11 +2,13 @@ import {
   Box,
   Button,
   ButtonBase,
+  CircularProgress,
   Grid,
   IconButton,
   List,
   makeStyles,
   Paper,
+  TablePagination,
   TextField,
   Theme,
   Typography,
@@ -15,32 +17,90 @@ import {
 import { Apps, ListAlt } from '@material-ui/icons'
 import { setNotification, NotificationTypes } from '@src/redux/data/notificationSlice'
 import { userDataSelector } from '@src/redux/data/userSlice'
-import { getRetailers, getSubdistributor } from '@src/utils/api/subdistributorApi'
 import userApi, { getUser, UserResponse } from '@src/utils/api/userApi'
-import { useEffect, useState } from 'react'
+import { ChangeEvent, ChangeEventHandler, useEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import useSWR from 'swr'
 import Image from 'next/image'
+import GridView from '@src/components/ECommerce/GridView'
+import { getAllInventory, getCommerce, Inventory } from '@src/utils/api/inventoryApi'
+import { Paginated, PaginateFetchParameters } from '@src/utils/types/PaginatedEntity'
+import { CaesarWalletResponse, getWallet, getWalletById } from '@src/utils/api/walletApi'
+import RowView from '@src/components/ECommerce/RowView'
+import type { InventoryNumbers } from '@src/components/ECommerce/RowView'
+import { UserTypesAndUser } from '@src/pages/admin/accounts'
+import { getDsp } from '@src/utils/api/dspApi'
+import { getAllAdmin } from '@src/utils/api/adminApi'
+import CreatePurchase from '@src/components/pages/transactions/CreatePurchase'
+import ModalWrapper from '@src/components/ModalWrapper'
+import LoadingScreen from '@src/components/LoadingScreen'
+getAllInventory()
 
-export default function ECommerce({ account }: { account: UserResponse | undefined }) {
+export default function ECommerce({
+  // account,
+  caesarBuyer,
+}: {
+  caesarBuyer: [UserTypesAndUser, string]
+}) {
+  const [account_type, caesar_id] = caesarBuyer
+  const user = useSelector(userDataSelector)
   const [view, setView] = useState({
     rowView: true,
     gridView: false,
   })
+  const [paginationParameters, setPaginationParameters] = useState<PaginateFetchParameters>({
+    limit: 100,
+    page: 0,
+  })
 
-  const { data: retailer, error } = useSWR(
-    [account?.subdistributor?.id, 'subdistributor-retailers'],
-    getRetailers
-  )
-  const [content, setContent] = useState(retailer)
+  const [paginateParams, setPaginateParams] = useState<PaginateFetchParameters>({
+    limit: 100,
+    page: 0,
+  })
 
-  console.log(retailer)
+  const [paginatedInventory, setPaginatedInventory] = useState<Paginated<Inventory> | undefined>()
+  const inventory = useMemo(() => paginatedInventory?.data, [paginatedInventory])
+  const inventoryMetadata = useMemo(() => paginatedInventory?.metadata, [paginatedInventory])
 
+  const srpKey: keyof InventoryNumbers = useMemo(() => {
+    switch (account_type) {
+      case 'dsp': {
+        return 'srp_for_dsp'
+      }
+      case 'retailer': {
+        return 'srp_for_retailer'
+      }
+      case 'subdistributor': {
+        return 'srp_for_subd'
+      }
+      case 'user': {
+        return 'srp_for_user'
+      }
+      default:
+        return 'unit_price'
+    }
+  }, [account_type])
+
+  useEffect(() => {
+    if (caesar_id) {
+      getCommerce({
+        ...paginateParams,
+        caesar: caesar_id,
+      }).then((res) => {
+        setPaginatedInventory(res)
+      })
+    }
+  }, [paginateParams, caesar_id])
+
+  const [selectedInventory, setSelectedInventory] = useState<undefined | Inventory>()
+  const [purchaseModalOpen, setPurchaseModalOpen] = useState(false)
   return (
     <div>
       <Paper
         style={{
           height: 'max-content',
+          maxHeight: 720,
+          overflowY: 'auto',
         }}
         variant="outlined"
       >
@@ -54,7 +114,7 @@ export default function ECommerce({ account }: { account: UserResponse | undefin
                       setView({ rowView: true, gridView: false })
                     }}
                   >
-                    <ListAlt fontSize="large" color={view.rowView ? 'primary' : undefined} />
+                    <ListAlt color={view.rowView ? 'primary' : undefined} />
                   </IconButton>
                 </Grid>
                 <Grid item>
@@ -63,87 +123,67 @@ export default function ECommerce({ account }: { account: UserResponse | undefin
                       setView({ rowView: false, gridView: true })
                     }}
                   >
-                    <Apps fontSize="large" color={view.gridView ? 'primary' : undefined} />
+                    <Apps color={view.gridView ? 'primary' : undefined} />
                   </IconButton>
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-
-          <Grid flex-direction="rows" container spacing={2}>
-            {view.rowView &&
-              retailer?.data?.map((value) => (
-                <Grid item xs={12} md={4} key={value.id}>
-                  <Paper variant="outlined">
-                    <Box p={2}>
-                      <Grid container spacing={1}>
-                        <Grid item xs={2}>
-                          <Image src="/apple-touch-icon.png" width={100} height={100} />
-                        </Grid>
-                        <Grid item xs={10}>
-                          <Grid container spacing={1}>
-                            <Grid item xs={10} md={8}>
-                              <Typography variant="h5">Title: {value.user.first_name} </Typography>
-                            </Grid>
-                            <Grid item xs={2} md={4}>
-                              <Typography variant="h5" noWrap>
-                                Price: {value.user.phone_number}
-                              </Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                              <Typography variant="body1" noWrap>
-                                Seller: {value.user.last_name}
-                              </Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                              <Typography variant="body1" noWrap>
-                                Quantity: {value.user.created_at}
-                              </Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                              <Typography variant="body1" noWrap>
-                                Description: {value.user.id}
-                              </Typography>
-                            </Grid>
-                          </Grid>
-                        </Grid>
-                      </Grid>
-                    </Box>
-                  </Paper>
-                </Grid>
-              ))}
-            {view.gridView &&
-              retailer?.data?.map((value) => (
-                <Grid item xs={4} md={3} lg={2} key={value.id}>
-                  <Paper variant="outlined">
-                    <Box p={2}>
-                      <Grid container justifyContent="center">
-                        <Grid item xs={10}>
-                          <Image src="/apple-touch-icon.png" width={150} height={150} />
-                        </Grid>
-                        <Grid item xs={12}>
-                          <Typography variant="h6" noWrap>
-                            Title: {value.user.first_name}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                          <Typography variant="body2" noWrap>
-                            Price: {value.user.phone_number}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                          <Typography variant="body2" noWrap>
-                            Qty: {value.user.created_at}
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                    </Box>
-                  </Paper>
-                </Grid>
-              ))}
-          </Grid>
+          {/* {view.rowView && inventory && <RowView inventory={inventory} />} */}
+          {/* {view.gridView && inventory && <GridView inventory={inventory} />} */}
+          {inventory && (
+            <RowView
+              srpKey={srpKey}
+              inventory={inventory}
+              onInventoryItemClick={(inventory: Inventory) => {
+                setSelectedInventory(inventory)
+                // setPurchaseModalOpen(true)
+              }}
+            />
+          )}
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 20, 50, 100]}
+            count={inventoryMetadata?.total || 0}
+            rowsPerPage={inventoryMetadata?.limit || 5}
+            page={inventoryMetadata?.page || 0}
+            onPageChange={(_, page) => {
+              setPaginateParams((prev) => ({
+                ...prev,
+                page,
+              }))
+            }}
+            onRowsPerPageChange={(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+              setPaginateParams((prev) => ({
+                ...prev,
+                limit: Number(e.target.value),
+              }))
+            }}
+            component="div"
+          />
         </Box>
       </Paper>
+
+      <ModalWrapper
+        containerSize="sm"
+        open={!!selectedInventory}
+        onClose={() => {
+          // setPurchaseModalOpen(false)
+          setSelectedInventory(undefined)
+        }}
+      >
+        {selectedInventory ? (
+          <CreatePurchase
+            buyerCaesarId={caesar_id}
+            inventory={selectedInventory}
+            srpKey={srpKey}
+            modal={() => {
+              setSelectedInventory(undefined)
+            }}
+          />
+        ) : (
+          <CircularProgress />
+        )}
+      </ModalWrapper>
     </div>
   )
 }
