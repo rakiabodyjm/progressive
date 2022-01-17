182b8af791916f0867b8ce88389dc4894e622c26
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-01-17 19:27:55 +0800

    Additional walletApi functions

--- src/utils/api/walletApi.ts
+++ src/utils/api/walletApi.ts
@@ -1,6 +1,12 @@
-/* eslint-disable no-redeclare */
-import { UserRoles, UserTypes } from '@src/redux/data/userSlice'
+import type { UserTypesAndUser } from '@src/pages/admin/accounts'
+import type { UserRoles, UserTypes } from '@src/redux/data/userSlice'
+import { AdminResponseType } from '@src/utils/api/adminApi'
 import { extractMultipleErrorFromResponse } from '@src/utils/api/common'
+import type { DspResponseType } from '@src/utils/api/dspApi'
+import type { RetailerResponseType } from '@src/utils/api/retailerApi'
+import { SubdistributorResponseType } from '@src/utils/api/subdistributorApi'
+import type { UserResponse } from '@src/utils/api/userApi'
+import { Paginated, PaginateFetchParameters } from '@src/utils/types/PaginatedEntity'
 import axios from 'axios'
 
 interface ExternalCeasar {
@@ -24,15 +30,25 @@ export interface CaesarWalletResponse {
 
   account_type: UserRoles
 
-  account_id: string
-
   ceasar_id: string
+
+  description: string
 
   data?: ExternalCeasar
 
   created_at: Date
 
   updated_at: Date
+
+  user?: UserResponse
+
+  subdistributor?: SubdistributorResponseType
+
+  dsp?: DspResponseType
+
+  retailer?: RetailerResponseType
+
+  admin?: AdminResponseType
 }
 
 export function createWallet(param: Record<UserTypes, string>): Promise<CaesarWalletResponse> {
@@ -41,7 +57,9 @@ export function createWallet(param: Record<UserTypes, string>): Promise<CaesarWa
       ...param,
     })
     .then((res) => res.data)
+
     .catch((err) => {
+      console.log('error from creatingwallet', err.response)
       throw extractMultipleErrorFromResponse(err)
     })
 }
@@ -55,10 +73,15 @@ export function getWalletById(id: string): Promise<CaesarWalletResponse> {
     })
 }
 
-export function getWallet<T = string>(...arg: T[]): Promise<CaesarWalletResponse>
+export function getWallet<T = string>(
+  params: UserTypesAndUser,
+  value: string
+): Promise<CaesarWalletResponse>
+// eslint-disable-next-line no-redeclare
 export function getWallet<T = Record<UserTypes, string>>(params: T): Promise<CaesarWalletResponse>
+// eslint-disable-next-line no-redeclare
 export function getWallet(
-  params: Record<UserTypes, string>,
+  params: UserTypesAndUser | Record<UserTypes, string>,
   value?: string
 ): Promise<CaesarWalletResponse> {
   if (typeof params === 'string') {
@@ -78,6 +101,23 @@ export function getWallet(
       params,
     })
     .then((res) => res.data)
+    .catch((err) => {
+      throw extractMultipleErrorFromResponse(err)
+    })
+}
+
+export type SearchWalletParams = {
+  searchQuery: string
+} & PaginateFetchParameters
+export function searchWallet(searchParams: SearchWalletParams) {
+  const { searchQuery } = searchParams
+  return axios
+    .get('/caesar/search', {
+      params: {
+        ...searchParams,
+      },
+    })
+    .then((res) => res.data as Paginated<CaesarWalletResponse>)
     .catch((err) => {
       throw extractMultipleErrorFromResponse(err)
     })
