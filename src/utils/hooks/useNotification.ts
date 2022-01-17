2fcc36ad8cb9ef17c8166fa41db69fb429a93b86
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-01-17 19:28:13 +0800

    useNotification hook component

--- src/utils/hooks/useNotification.ts
+++ src/utils/hooks/useNotification.ts
@@ -1,4 +1,5 @@
 import { NotificationTypes, setNotification } from '@src/redux/data/notificationSlice'
+import { useCallback } from 'react'
 import { useDispatch } from 'react-redux'
 
 type DispatchNotification = {
@@ -7,34 +8,44 @@ type DispatchNotification = {
 }
 export default function useNotification() {
   const dispatch = useDispatch()
-  return ({ message, type }: DispatchNotification) =>
-    dispatch(
-      setNotification({
-        message,
-        type,
-      })
-    )
+  return useCallback(
+    ({ message, type }: DispatchNotification) =>
+      dispatch(
+        setNotification({
+          message,
+          type,
+        })
+      ),
+    [dispatch]
+  )
   //   return dispatch(setNotification(payload))
 }
 
 export function useSuccessNotification() {
   const dispatch = useDispatch()
-  return (message: string) =>
-    dispatch(
-      setNotification({
-        message,
-        type: NotificationTypes.SUCCESS,
-      })
-    )
+  return useCallback(
+    (message: string) =>
+      dispatch(
+        setNotification({
+          message,
+          type: NotificationTypes.SUCCESS,
+        })
+      ),
+    [dispatch]
+  )
 }
 
 export function useErrorNotification() {
   const dispatch = useDispatch()
-  return (message: string) =>
-    dispatch(
-      setNotification({
-        message,
-        type: NotificationTypes.ERROR,
-      })
-    )
+
+  return useCallback(
+    (message: string) =>
+      dispatch(
+        setNotification({
+          message,
+          type: NotificationTypes.ERROR,
+        })
+      ),
+    [dispatch]
+  )
 }
