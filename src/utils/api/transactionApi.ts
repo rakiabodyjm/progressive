d8b82a9b5d10b7b516276478d14a4ad4dc2702b9
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-01-19 18:11:08 +0800

    TransactionAPI for ECommerce

--- src/utils/api/transactionApi.ts
+++ src/utils/api/transactionApi.ts
@@ -1,8 +1,11 @@
 import { UserTypesAndUser } from '@src/pages/admin/accounts'
+import { extractMultipleErrorFromResponse } from '@src/utils/api/common'
 import { Inventory } from '@src/utils/api/inventoryApi'
 import { CaesarWalletResponse } from '@src/utils/api/walletApi'
+import { Paginated, PaginateFetchParameters } from '@src/utils/types/PaginatedEntity'
+import axios from 'axios'
 
-interface TransactionResponse {
+export interface TransactionResponse {
   id: string
 
   inventory_from: Inventory
@@ -30,4 +33,112 @@ interface TransactionResponse {
   seller_profit: number
 
   approved: boolean
+}
+
+export interface PendingTransactionResponse {
+  id: string
+
+  pending_purchase_id: string
+
+  approved: boolean
+
+  inventory: Inventory
+
+  caesar_buyer: CaesarWalletResponse
+
+  caesar_seller: CaesarWalletResponse
+
+  quantity: number
+
+  transaction_id?: TransactionResponse
+
+  created_at: Date
+
+  updated_at: Date
+
+  deleted_at: Date
+}
+
+export function createPurchase({
+  inventoryId,
+  buyerCaesarId,
+  quantity,
+}: {
+  inventoryId: string
+  buyerCaesarId: string
+  quantity: number
+}): Promise<{
+  transaction?: TransactionResponse
+  pending_transactions?: PendingTransactionResponse[]
+}> {
+  return axios
+    .post(`/transaction/purchase/${inventoryId}/${buyerCaesarId}`, {
+      quantity,
+    })
+    .then(
+      (res) =>
+        res.data as {
+          transaction?: TransactionResponse
+          pending_transactions?: PendingTransactionResponse[]
+        }
+    )
+    .catch((err) => {
+      throw extractMultipleErrorFromResponse(err)
+    })
+}
+
+/**
+ * 
+    limit,
+    caesar,
+    buyer,
+    seller,
+    inventory_from,
+    inventory_to,
+    inventory,
+ */
+
+type GetAllTransactionsByCaesar =
+  | {
+      caesar: string
+      buyer: never
+      seller: never
+    }
+  | {
+      buyer?: string
+      seller?: string
+      caesar: never
+    }
+type GetAllTransactionsByInventory =
+  | {
+      inventory: string
+      inventory_from?: never
+      inventory_to?: never
+    }
+  | {
+      inventory?: never
+      inventory_from?: string
+      inventory_to?: string
+    }
+// type GetAllTransactionsByInventoryDirection = {
+//   inventory_from?: string
+//   inventory_to?: string
+// }
+// type GetAllTransactionsByCaesarDirection = {
+//   buyer?: string
+//   seller?: string
+// }
+export function getAllTransactions(
+  params: GetAllTransactionsByCaesar & GetAllTransactionsByInventory & PaginateFetchParameters
+): Promise<Paginated<TransactionResponse>> {
+  return axios
+    .get('/transactions', {
+      params: {
+        ...params,
+      },
+    })
+    .then((res) => res.data)
+    .catch((err) => {
+      throw extractMultipleErrorFromResponse(err)
+    })
 }
