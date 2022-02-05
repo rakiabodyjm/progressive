d5790148dbb0c22d924e0fdd3619fafdab27d7f7
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-02-05 09:43:43 +0800

    CaesarTabs for Rendering Current users caesar to be used

---
+++ src/components/CaesarTabs/index.tsx
@@ -0,0 +1,144 @@
+import {
+  Box,
+  Container,
+  Divider,
+  Paper,
+  Tab,
+  Tabs,
+  Theme,
+  Typography,
+  useTheme,
+} from '@material-ui/core'
+import { grey } from '@material-ui/core/colors'
+import { UserTypesAndUser } from '@src/pages/admin/accounts'
+import { userDataSelector } from '@src/redux/data/userSlice'
+import { getWallet } from '@src/utils/api/walletApi'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+/**
+ *
+ * Loads Available Caesar based on global user
+ */
+export default function CaesarTabs({
+  onActiveCaesarChange,
+}: {
+  onActiveCaesarChange: (e: [UserTypesAndUser, string] | undefined) => void
+}) {
+  const user = useSelector(userDataSelector)
+  const [caesarTypes, setCaesarTypes] = useState<[UserTypesAndUser, string][]>([])
+  const [activeCaesar, setActiveCaesar] = useState<[UserTypesAndUser, string] | undefined>()
+  const theme: Theme = useTheme()
+
+  useEffect(() => {
+    if (user && user.roles && user?.roles?.length > 0) {
+      /**
+       * verify if caeasar exists for each
+       */
+      const getWallets = () =>
+        Promise.all(
+          [...user.roles]
+            /**
+             * disable users for now
+             */
+            .filter((ea) => ea !== 'user')
+            .map((role) =>
+              getWallet({
+                [role]: user[`${role}_id`],
+              })
+                .then((res) => [res.account_type, res.id] as [UserTypesAndUser, string])
+                .catch((err) => [role, null])
+            )
+        ).then((final) => final.filter((ea) => !!ea[1]) as [UserTypesAndUser, string][])
+
+      getWallets()
+        .then((res) => {
+          setCaesarTypes(res)
+          if (res.length > 0) {
+            setActiveCaesar(res[0])
+            onActiveCaesarChange(res[0])
+          }
+        })
+        .catch((err) => {
+          console.log('No Caesars for', err)
+        })
+    }
+  }, [user])
+
+  return (
+    <>
+      <Paper>
+        <Box>
+          {/* {typeof tabTitle === 'string' ? (
+          ) : (
+            { tabTitle }
+          )}
+          {typeof tabSubTitle === 'string' ? (
+            <Typography variant="body2" color="primary">
+              {tabSubTitle}
+            </Typography>
+          ) : (
+            { tabSubTitle }
+          )} */}
+
+          {activeCaesar ? (
+            <>
+              <Box p={1} pb={0}>
+                <Typography
+                  style={{
+                    fontWeight: 700,
+                  }}
+                  //   color="textSecondary"
+                  align="center"
+                  variant="body2"
+                  color="primary"
+                >
+                  Select Caesar Account
+                </Typography>
+                <Tabs
+                  indicatorColor="primary"
+                  textColor="primary"
+                  onChange={(_, value) => {
+                    const result = caesarTypes.find((ea) => ea[0] === value)
+                    setActiveCaesar(result)
+                    onActiveCaesarChange(result)
+                  }}
+                  value={activeCaesar[0]}
+                  centered
+                >
+                  {caesarTypes.map(([caesarType]) => (
+                    <Tab
+                      key={caesarType}
+                      value={caesarType}
+                      label={caesarType?.toUpperCase()}
+                    ></Tab>
+                  ))}
+                </Tabs>
+              </Box>
+            </>
+          ) : (
+            <>
+              <Paper
+                style={{
+                  background: theme.palette.type === 'dark' ? grey['900'] : grey['200'],
+                }}
+              >
+                <Box p={1}>
+                  <Typography
+                    style={{
+                      fontWeight: 700,
+                    }}
+                    color="textSecondary"
+                    align="center"
+                    variant="body2"
+                  >
+                    No Caesar Account Found
+                  </Typography>
+                </Box>
+              </Paper>
+            </>
+          )}
+        </Box>
+      </Paper>
+    </>
+  )
+}
