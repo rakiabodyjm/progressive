f54b2de694e6f2010f685edff2ac5f6ae566bcbb
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-03-08 02:03:11 +0000

    Alpha environment variables and configurations

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,20 @@
 module.exports = {
   apps: [
     {
-      name: 'REALM1000 Telco',
+      name: 'REALM1000 ALPHA FRONTEND',
       script: './server.js',
       exec_mode: 'cluster',
       instances: 2,
       autorestart: false,
       env_production: {
         NODE_ENV: 'production',
+        BACKEND_HOSTNAME: `api.caesarcoin.ph`,
+        NEXT_PUBLIC_BACKEND_URL: `https://api.caesarcoin.ph`,
+      },
+      env: {
+        NODE_ENV: 'development',
+        BACKEND_HOSTNAME: `api.caesarcoin.ph`,
+        NEXT_PUBLIC_BACKEND_URL: `https://api.caesarcoin.ph`,
       },
     },
   ],
