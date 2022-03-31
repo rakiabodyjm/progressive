904d079d2d37258697a7dafde6d9ea70e97d68c4
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-03-31 18:15:11 +0800

    Fix Unlimited rerendering in /ecommerce page

--- src/pages/ecommerce/index.tsx
+++ src/pages/ecommerce/index.tsx
@@ -2,10 +2,14 @@ import { Paper, Box, Grid, Typography, Divider } from '@material-ui/core'
 import CaesarTabs from '@src/components/CaesarTabs'
 import ECommerce from '@src/components/ECommerce'
 import { UserTypes } from '@src/redux/data/userSlice'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 export default function ECommercePage() {
   const [caesar, setCaesar] = useState<[UserTypes, string] | undefined>()
+
+  const onActiveCaesarChange = useCallback((caesarActive, accountActive) => {
+    setCaesar(caesarActive)
+  }, [])
   return (
     <div>
       <Paper>
@@ -22,13 +26,8 @@ export default function ECommercePage() {
               </Box>
             </Grid>
             <Grid item xs={12}>
-              <CaesarTabs
-                onActiveCaesarChange={(caesarActive, accountActive) => {
-                  setCaesar(caesarActive)
-                }}
-              />
+              <CaesarTabs onActiveCaesarChange={onActiveCaesarChange} />
             </Grid>
-            {console.log(caesar)}
             {caesar && (
               <Grid item xs={12}>
                 <Box my={2} />
