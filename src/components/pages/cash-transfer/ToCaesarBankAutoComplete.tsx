b9cfb8b081e17bae4d5f0c1ef2408e3bee96565b
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-12 04:18:39 +0800

    ToCaesarBankAutoComplete renderOption, include caesarAccount description from list

--- src/components/pages/cash-transfer/ToCaesarBankAutoComplete.tsx
+++ src/components/pages/cash-transfer/ToCaesarBankAutoComplete.tsx
@@ -1,3 +1,4 @@
+import { Box, Typography } from '@material-ui/core'
 import SimpleAutoComplete, { SimpleAutoCompleteProps } from '@src/components/SimpleAutoComplete'
 import { CaesarBank } from '@src/utils/types/CashTransferTypes'
 import axios from 'axios'
@@ -24,9 +25,17 @@ const ToCaesarBankAutoComplete = ({
   <SimpleAutoComplete<CaesarBank, string | undefined>
     initialQuery={undefined}
     fetcher={(params) => searchCaesarBank(params).then((res) => (filter ? filter(res) : res))}
-    getOptionLabel={(option) => `${option.id} | ${option.description}`}
+    getOptionLabel={(option) => `${option.description} - ${option.caesar.description}`}
+    renderOption={(option) => (
+      <Box display="flex" flexDirection="column">
+        <Typography display="block" variant="body2">{`${option.description}`}</Typography>
+        <Typography variant="caption" color="primary">
+          {option.caesar.description}{' '}
+        </Typography>
+      </Box>
+    )}
     getOptionSelected={(val1, val2) => val1.id === val2.id}
-    querySetter={(param) => param}
+    querySetter={(param, input) => input}
     onChange={onChange}
     {...restProps}
   />
