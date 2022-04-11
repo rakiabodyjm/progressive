4e019fc7390b3c8303221dd656be709d2eb6922a
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-12 04:19:23 +0800

    ToCaesarAutoComplete add optional filter function prop

--- src/components/pages/cash-transfer/ToCaesarAutoComplete.tsx
+++ src/components/pages/cash-transfer/ToCaesarAutoComplete.tsx
@@ -5,32 +5,25 @@ import { useEffect } from 'react'
 
 const ToCaesarAutoComplete = ({
   onChange,
+  filter,
   ...restProps
 }: {
   onChange: (param: CaesarWalletResponse) => void
+  filter?: (arg: CaesarWalletResponse[]) => CaesarWalletResponse[]
 } & Partial<SimpleAutoCompleteProps<CaesarWalletResponse, string | undefined>>) => (
   <>
     <SimpleAutoComplete<CaesarWalletResponse, string | undefined>
       {...restProps}
       initialQuery={undefined}
-      querySetter={(_, inputValue) => {
-        console.log('inputValue', inputValue, typeof inputValue)
-        return inputValue
-      }}
+      querySetter={(_, inputValue) => inputValue}
       fetcher={(params) =>
         searchWalletV2({
           page: 0,
           limit: 100,
           searchQuery: params && params?.length > 0 ? params : undefined,
         })
-          .then((res) => {
-            console.log('res', res.data)
-            return res.data
-          })
-          .catch((err) => {
-            console.log('error', err)
-            return []
-          })
+          .then((res) => (filter ? filter(res.data) : res.data))
+          .catch((err) => [])
       }
       onChange={onChange}
       getOptionLabel={(option) => option.description}
