b77265855849780f608b90444f9c0e808bf62e80
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-14 04:22:01 +0800

    CashTransferList for rendering transactions and details

---
+++ src/components/pages/cash-transfer/CashTransferList.tsx
@@ -0,0 +1,257 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-nested-ternary */
+import { Box, ListItem, Paper, Theme, Typography } from '@material-ui/core'
+import { grey } from '@material-ui/core/colors'
+import { useTheme } from '@material-ui/styles'
+import FormLabel from '@src/components/FormLabel'
+import { LoadingScreen2 } from '@src/components/LoadingScreen'
+import { formatIntoCurrency } from '@src/utils/api/common'
+import { CaesarWalletResponse } from '@src/utils/api/walletApi'
+import {
+  CaesarBank,
+  CashTransferAs,
+  CashTransferResponse,
+} from '@src/utils/types/CashTransferTypes'
+import { Paginated } from '@src/utils/types/PaginatedEntity'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { useCallback, useState } from 'react'
+import useSWR from 'swr'
+
+export default function CashTransferList({
+  caesarId,
+  caesarBankId,
+}: {
+  caesarId?: CaesarWalletResponse['id']
+  caesarBankId?: CaesarBank['id']
+}) {
+  const [queryParameters, setQueryParameters] = useState<{
+    page: number
+    limit: number
+  }>({
+    page: 0,
+    limit: 100,
+  })
+  const {
+    data: cashTransfers,
+    isValidating: loadingCashTransfers,
+    error: errorCashTransfers,
+  } = useSWR(caesarId || caesarBankId ? ['/cash-transfer', queryParameters] : null, (url, params) =>
+    axios
+      .get(url, {
+        params: {
+          ...params,
+          caesar: caesarId,
+          caesar_bank: caesarBankId,
+          //   caesar_bank_from: caesarBankId,
+          //   caesar_bank_to: caesarBankId,
+        },
+      })
+      .then((res) => res.data as Paginated<CashTransferResponse>)
+      .catch((err) => {
+        console.log('failed loading cash-transfers', err)
+      })
+  )
+
+  const isSender = useCallback(
+    (cashTransfer: CashTransferResponse) =>
+      !!(
+        (caesarBankId && cashTransfer?.caesar_bank_from?.id === caesarBankId) ||
+        (caesarId && cashTransfer?.from?.id === caesarId) ||
+        cashTransfer?.caesar_bank_from?.caesar.id === caesarId
+      ),
+    [caesarId, caesarBankId]
+  )
+  const theme: Theme = useTheme()
+  const router = useRouter()
+  return (
+    <>
+      <Box
+        style={{
+          display: 'grid',
+          gap: 8,
+          maxHeight: 400,
+          overflowY: 'auto',
+        }}
+      >
+        {!errorCashTransfers && cashTransfers?.data && cashTransfers?.data.length > 0 ? (
+          <>
+            {cashTransfers.data.map((cashTransfer) => (
+              <Paper
+                key={cashTransfer.id}
+                style={{
+                  overflow: 'hidden',
+                }}
+                variant="outlined"
+              >
+                <ListItem
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                    padding: 8,
+                  }}
+                  button
+                  onClick={
+                    cashTransfer.as === CashTransferAs.LOAN
+                      ? () => {
+                          router.push(`/cash-transfer/loan/${cashTransfer?.id}`)
+                        }
+                      : undefined
+                  }
+                >
+                  <Box display="flex" width="100%" justifyContent="space-between">
+                    <Typography
+                      style={{
+                        display: 'block',
+                        alignSelf: 'flex-end',
+                      }}
+                      variant="caption"
+                    >
+                      {cashTransfer.id.split('-')[0]}
+                    </Typography>
+                    <Typography
+                      style={{
+                        display: 'block',
+                        alignSelf: 'flex-end',
+                      }}
+                      variant="caption"
+                    >
+                      {new Date(cashTransfer.created_at).toLocaleString()}
+                    </Typography>
+                  </Box>
+
+                  <Typography>
+                    {isSender(cashTransfer)
+                      ? cashTransfer?.caesar_bank_to?.description || cashTransfer?.to?.description
+                      : cashTransfer?.caesar_bank_from?.description ||
+                        cashTransfer?.from?.description ||
+                        'ERROR'}
+                    {/* {cashTransfer?.to?.description ||
+                      cashTransfer?.caesar_bank_to?.description ||
+                      'ERROR'} */}
+                  </Typography>
+                  <Box
+                    display="flex"
+                    width="100%"
+                    alignItems="flex-end"
+                    justifyContent="space-between"
+                  >
+                    <Typography variant="body2" color="primary">
+                      {cashTransfer.as}
+                    </Typography>
+                    <Typography
+                      style={{
+                        alignSelf: 'flex-end',
+                      }}
+                    >
+                      <Typography component="span">
+                        {cashTransfer?.as !== CashTransferAs.DEPOSIT && isSender(cashTransfer)
+                          ? ' - '
+                          : ' + '}
+                      </Typography>
+
+                      {formatIntoCurrency(cashTransfer.amount)}
+                    </Typography>
+                  </Box>
+                  {cashTransfer?.as !== CashTransferAs.DEPOSIT && isSender(cashTransfer) && (
+                    <Box
+                      display="flex"
+                      width="100%"
+                      alignItems="flex-end"
+                      justifyContent="space-between"
+                    >
+                      <Typography variant="caption" color="textSecondary">
+                        {/* {cashTransfer.as} */}
+                        Bank Fee:
+                      </Typography>
+                      <Typography
+                        variant="caption"
+                        style={{
+                          alignSelf: 'flex-end',
+                        }}
+                      >
+                        - {formatIntoCurrency(cashTransfer?.bank_charge)}
+                      </Typography>
+                    </Box>
+                  )}
+                  {!isSender(cashTransfer) && (
+                    <Box
+                      display="flex"
+                      width="100%"
+                      alignItems="flex-end"
+                      justifyContent="space-between"
+                    >
+                      <Typography variant="caption" color="textSecondary">
+                        {/* {cashTransfer.as} */}
+                        Sender's Bank Fee:
+                      </Typography>
+                      <Typography
+                        variant="caption"
+                        style={{
+                          alignSelf: 'flex-end',
+                        }}
+                      >
+                        + {formatIntoCurrency(cashTransfer?.bank_charge)}
+                      </Typography>
+                    </Box>
+                  )}
+
+                  <Box
+                    display="flex"
+                    width="100%"
+                    alignItems="flex-end"
+                    justifyContent="space-between"
+                  >
+                    <Typography variant="caption" color="textSecondary">
+                      {/* {cashTransfer.as} */}
+                      Remaining Bank Balance
+                    </Typography>
+                    <Typography
+                      variant="caption"
+                      style={{
+                        alignSelf: 'flex-end',
+                      }}
+                    >
+                      {formatIntoCurrency(
+                        isSender(cashTransfer) && cashTransfer?.as !== CashTransferAs.DEPOSIT
+                          ? cashTransfer.remaining_balance_from
+                          : cashTransfer.remaining_balance_to
+                      )}
+                    </Typography>
+
+                    {}
+                  </Box>
+                </ListItem>
+              </Paper>
+            ))}
+          </>
+        ) : loadingCashTransfers ? (
+          <LoadingScreen2
+            containerProps={{
+              style: {
+                minHeight: 120,
+              },
+            }}
+          />
+        ) : (
+          <Paper
+            style={{
+              minHeight: 120,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              background: theme.palette.type === 'dark' ? grey['900'] : grey['200'],
+              flexDirection: 'column',
+            }}
+          >
+            <Typography variant="body2">Not Available</Typography>
+            <Typography variant="caption" color="primary">
+              No Recent Transactions
+            </Typography>
+          </Paper>
+        )}
+      </Box>
+    </>
+  )
+}
