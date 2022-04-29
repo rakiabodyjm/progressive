054bf1189fd044c69bda5068afd5eea058e88523
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-29 11:38:40 +0800

    Add Loan and Loan Payment in CashTransferForm

--- src/components/pages/cash-transfer/CashTransferForm.tsx
+++ src/components/pages/cash-transfer/CashTransferForm.tsx
@@ -1,43 +1,76 @@
 import { Box, Paper } from '@material-ui/core'
+import { LoadingScreen2 } from '@src/components/LoadingScreen'
 import { CaesarWalletResponse } from '@src/utils/api/walletApi'
-import { CaesarBank } from '@src/utils/types/CashTransferTypes'
-import dynamic from 'next/dynamic'
+import {
+  CaesarBank,
+  CashTransferAs,
+  CashTransferResponse,
+} from '@src/utils/types/CashTransferTypes'
+import dynamic, { DynamicOptions, Loader } from 'next/dynamic'
+import { ReactComponentElement } from 'react'
 
-const TransferTypeTransaction = dynamic(
+function dynamicImportWithLoading<T>(param: T) {
+  return dynamic(param, {
+    loading: () => <LoadingScreen2 />,
+  })
+}
+
+const TransferTypeTransaction = dynamicImportWithLoading(
   () => import('@src/components/pages/cash-transfer/CreateTransferTypeTransaction')
 )
-const WithDrawTypeTransaction = dynamic(
+const WithDrawTypeTransaction = dynamicImportWithLoading(
   () => import('@src/components/pages/cash-transfer/CreateWithdrawTypeTransaction')
 )
 
-const DepositTypeTransaction = dynamic(
+const DepositTypeTransaction = dynamicImportWithLoading(
   () => import('@src/components/pages/cash-transfer/CreateDepositTypeTransaction')
+)
+
+const LoanTypeTransaction = dynamicImportWithLoading(
+  () => import('@src/components/pages/cash-transfer/CreateLoanTypeTransaction')
+)
+
+const LoanPaymentTransaction = dynamicImportWithLoading(
+  () => import('@src/components/pages/cash-transfer/CreateLoanPaymentTypeTransaction')
 )
 
 type TransferTypeProps = {
   caesar_bank_from?: CaesarBank
   caesar_bank_to?: CaesarBank
-  transactionType: 'transfer'
+  transactionType: CashTransferAs.TRANSFER
 }
 type WithdrawTypeProps = {
   caesar_bank_from?: CaesarBank
   to?: CaesarWalletResponse
-  transactionType: 'withdraw'
+  transactionType: CashTransferAs.WITHDRAW
 }
 
 type DepositTypeProps = {
   caesar_bank_to: CaesarBank
   from: CaesarWalletResponse
-  transactionType: 'deposit'
+  transactionType: CashTransferAs.DEPOSIT
 }
 
+type LoanTypeProps = {
+  caesar_bank_from?: CaesarBank
+  caesar_bank_to?: CaesarBank
+  transactionType: CashTransferAs.LOAN
+}
+
+type LoanPaymentTypeProps = {
+  transactionType: CashTransferAs.LOAN_PAYMENT
+  cash_transfer: CashTransferResponse
+}
 export default function CashTransferForm({
   transactionType,
   ...restProps
-}: Omit<TransferTypeProps | WithdrawTypeProps | DepositTypeProps, 'children'>) {
+}: Omit<
+  TransferTypeProps | WithdrawTypeProps | DepositTypeProps | LoanTypeProps | LoanPaymentTypeProps,
+  'children'
+>) {
   return (
     <>
-      {transactionType === 'transfer' && (
+      {transactionType === CashTransferAs.TRANSFER && (
         <Paper>
           <Box p={2}>
             <TransferTypeTransaction {...restProps} />
@@ -45,7 +78,7 @@ export default function CashTransferForm({
         </Paper>
       )}
 
-      {transactionType === 'withdraw' && (
+      {transactionType === CashTransferAs.WITHDRAW && (
         <Paper>
           <Box p={2}>
             <WithDrawTypeTransaction {...restProps} />
@@ -53,25 +86,31 @@ export default function CashTransferForm({
         </Paper>
       )}
 
-      {transactionType === 'deposit' && (
+      {transactionType === CashTransferAs.DEPOSIT && (
         <Paper>
           <Box p={2}>
             <DepositTypeTransaction {...restProps} />
           </Box>
         </Paper>
       )}
+
+      {transactionType === CashTransferAs.LOAN && (
+        <Paper>
+          <Box p={2}>
+            <LoanTypeTransaction {...restProps} />
+          </Box>
+        </Paper>
+      )}
+
+      {transactionType === CashTransferAs['LOAN PAYMENT'] && (
+        <Paper>
+          <Box p={2}>
+            <LoanPaymentTransaction {...restProps} />
+          </Box>
+        </Paper>
+      )}
     </>
   )
-}
-
-export enum CashTransferAs {
-  DEPOSIT = 'DEPOSIT',
-  TRANSFER = 'TRANSFER',
-  WITHDRAW = 'WITHDRAW',
-  LOAN = 'LOAN',
-  'LOAN PAYMENT' = 'LOAN PAYMENT',
-  INTEREST = 'INTEREST',
-  'BANK FEES' = 'BANK FEES',
 }
 
 // export { WithDrawTypeTransaction, DepositTypeTransaction }
