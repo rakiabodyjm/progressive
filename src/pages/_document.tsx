00cacee5e0be22694507061d58733d42a68a3edd
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-05-13 17:35:53 +0800

    Prepare for PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,7 +36,7 @@ class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ed8332" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ec5b2b" />
           <meta name="msapplication-TileColor" content="#0f254c" />
           <meta name="theme-color" content="#0f254c" />
         </Head>
