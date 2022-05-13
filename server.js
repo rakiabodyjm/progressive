baa50eb128881a49c3ba2fb33ea0ca4217c3b54a
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-05-14 02:34:49 +0800

    ServerJS PWA configurations

--- server.js
+++ server.js
@@ -8,14 +8,19 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 const port = process.env.PORT || 2006
+const { join } = require('path')
 app.prepare().then(() => {
   createServer((req, res) => {
     // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
-
-    handle(req, res, parsedUrl)
+    if (pathname === '/sw.js' || /^\/(workbox|worker|fallback)-\w+\.js$/.test(pathname)) {
+      const filePath = join(__dirname, 'public', pathname)
+      app.serveStatic(req, res, filePath)
+    } else {
+      handle(req, res, parsedUrl)
+    }
   }).listen(port, (err) => {
     if (err) throw err
     /**
