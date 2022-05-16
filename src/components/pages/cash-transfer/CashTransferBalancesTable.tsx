ee24e006c7f3daa8286ce78f41d99ae31427b9f5
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-05-17 03:05:37 +0800

    Create CashTransferBalancesTable for better UI in navigating overall balances of accounts

---
+++ src/components/pages/cash-transfer/CashTransferBalancesTable.tsx
@@ -0,0 +1,326 @@
+/* eslint-disable react/no-unescaped-entities */
+import {
+  Box,
+  BoxProps,
+  Paper,
+  PaperProps,
+  TablePagination,
+  Theme,
+  Typography,
+} from '@material-ui/core'
+import { grey } from '@material-ui/core/colors'
+import { useTheme } from '@material-ui/styles'
+import ErrorLoading from '@src/components/ErrorLoadingScreen'
+import FormLabel from '@src/components/FormLabel'
+import FormTextField from '@src/components/FormTextField'
+import { LoadingScreen2 } from '@src/components/LoadingScreen'
+import RoleBadge from '@src/components/RoleBadge'
+import UsersTable from '@src/components/UsersTable'
+import { userDataSelector, UserTypes } from '@src/redux/data/userSlice'
+import { formatIntoCurrency } from '@src/utils/api/common'
+import { CaesarWalletResponse, searchWalletV2 } from '@src/utils/api/walletApi'
+import useNotification from '@src/utils/hooks/useNotification'
+import { Bank, CaesarBank } from '@src/utils/types/CashTransferTypes'
+import { PaginateFetchParameters } from '@src/utils/types/PaginatedEntity'
+import { useRouter } from 'next/router'
+import { ChangeEvent, useCallback, useMemo, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
+import useSWR from 'swr'
+
+export const CashTransferBalancesTable = ({
+  disabledKeys,
+  paperProps,
+  boxProps,
+}: {
+  disabledKeys?: ('id' | 'name' | 'account_type' | 'bank_accounts' | 'balance' | 'bank_balances')[]
+  paperProps?: PaperProps
+  boxProps?: BoxProps
+}) => {
+  const user = useSelector(userDataSelector)
+  const isAuthorizedForViewingBalances = useMemo(
+    () => user && user.roles.some((ea) => ['ct-operator', 'ct-admin', 'admin'].includes(ea)),
+    [user]
+  )
+
+  const router = useRouter()
+
+  // const searchQuery = useState<undefined | string>('')
+  const [query, setQuery] = useState<PaginateFetchParameters & { searchQuery?: string }>({
+    limit: 100,
+    page: 0,
+    searchQuery: undefined,
+    ...(!isAuthorizedForViewingBalances && {
+      account_type: 'retailer',
+    }),
+  })
+  const setQueryState = useCallback(
+    (param: keyof typeof query) => (value: typeof query[keyof typeof query]) => {
+      setQuery((prev) => ({
+        ...prev,
+        [param]: value,
+      }))
+    },
+    []
+  )
+
+  const formatter = useCallback(
+    (param: CaesarWalletResponse[]) =>
+      param.map(({ id, description, account_type, cash_transfer_balance, bank_accounts }) => {
+        if (disabledKeys) {
+          return Object.entries({
+            id,
+            name: description,
+            account_type,
+            bank_accounts,
+            balance: formatIntoCurrency(cash_transfer_balance),
+            bank_balances: formatIntoCurrency(
+              bank_accounts.reduce((acc, ea) => acc + ea.balance, 0)
+            ),
+          }).reduce((acc, [key, value]) => {
+            if (
+              disabledKeys.includes(
+                key as
+                  | 'id'
+                  | 'name'
+                  | 'account_type'
+                  | 'bank_accounts'
+                  | 'balance'
+                  | 'bank_balances'
+              )
+            ) {
+              return { ...acc }
+            }
+            return {
+              ...acc,
+
+              [key]: value,
+            }
+          }, {} as Partial<Record<'id' | 'name' | 'account_type' | 'bank_accounts' | 'balance' | 'bank_balances', unknown>>)
+        }
+        const returnValue = {
+          id,
+          name: description,
+          account_type,
+          //   bank_accounts: bank_accounts.map((ea) => ea.bank.name).join(' '),
+          bank_accounts,
+          balance: formatIntoCurrency(cash_transfer_balance),
+          bank_balances: formatIntoCurrency(bank_accounts.reduce((acc, ea) => acc + ea.balance, 0)),
+        }
+        return returnValue
+      }),
+    []
+  )
+  const fetcher = useCallback(
+    () =>
+      searchWalletV2(query).then(async (res) => ({
+        metadata: res.metadata,
+        data: formatter(res.data),
+      })),
+    [query, formatter]
+  )
+
+  const { data: paginatedCaesar, isValidating, error } = useSWR(['/caesar', query], fetcher)
+  const caesars = useMemo(() => paginatedCaesar?.data || undefined, [paginatedCaesar])
+
+  const ceasarMetaData = useMemo(() => paginatedCaesar?.metadata || undefined, [paginatedCaesar])
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | undefined>()
+
+  const theme: Theme = useTheme()
+
+  if (error) {
+    return <ErrorLoading />
+  }
+  return (
+    <>
+      <Paper variant="outlined" {...paperProps}>
+        <Box p={2} {...boxProps}>
+          <FormLabel>Search for Accounts</FormLabel>
+          <Box my={1} />
+          <FormTextField
+            name="search"
+            onChange={(e) => {
+              if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current)
+              }
+              timeoutRef.current = setTimeout(() => {
+                setQuery((prev) => ({
+                  ...prev,
+                  // searchQuery: e.target.value?.length > 0 ? e.target.value : undefined,
+                  searchQuery: e.target.value,
+                }))
+              }, 500)
+            }}
+          />
+          <Box mt={1}>
+            {caesars?.length === 0 && !isValidating && (
+              <Paper
+                style={{
+                  minHeight: 120,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  background: theme.palette.type === 'dark' ? grey['900'] : grey['200'],
+                  flexDirection: 'column',
+                }}
+              >
+                <Typography variant="body2">No Account Matched the Search</Typography>
+                <Typography variant="caption" color="primary">
+                  Try Searching for a different keyword
+                </Typography>
+                <Typography variant="caption" color="textSecondary">
+                  Caesar Account's First Name, Last Name, Phone Number
+                </Typography>
+              </Paper>
+            )}
+            {isValidating ? (
+              <LoadingScreen2
+                containerProps={{
+                  style: {
+                    borderRadius: 4,
+                  },
+                }}
+              />
+            ) : (
+              caesars &&
+              caesars.length > 0 && (
+                <>
+                  <Box
+                    style={{
+                      maxHeight: 640,
+                      overflowY: 'auto',
+                    }}
+                  >
+                    {Object.entries(
+                      caesars.reduce((acc, ea) => {
+                        let accum = { ...acc }
+                        if (!accum[ea.account_type as keyof typeof accum]) {
+                          accum = {
+                            ...accum,
+                            [ea.account_type as keyof typeof accum]: [],
+                          }
+                        }
+                        accum[ea.account_type as keyof typeof accum] = [
+                          ...accum[ea.account_type as keyof typeof accum],
+                          ea,
+                        ]
+                        return accum
+                      }, {} as Record<UserTypes, ReturnType<typeof formatter>>)
+                    )
+                      ?.sort(([key1], [key2]) => key1.localeCompare(key2))
+                      .map(([accountType, caesarValues]) => (
+                        <Box
+                          style={{
+                            position: 'sticky',
+                            top: 0,
+                          }}
+                          key={accountType}
+                        >
+                          <Box
+                            style={{
+                              background: theme.palette.type === 'dark' ? grey['900'] : grey['200'],
+                              borderTopLeftRadius: 4,
+                              borderTopRightRadius: 4,
+                            }}
+                            p={2}
+                            // mt={2}
+                          >
+                            <RoleBadge disablePopUp uppercase>
+                              {accountType}
+                            </RoleBadge>
+                          </Box>
+                          <UsersTable
+                            data={caesarValues}
+                            limit={query.limit!}
+                            page={query.page!}
+                            setLimit={setQueryState('limit')}
+                            setPage={setQueryState('page')}
+                            total={ceasarMetaData?.total || 0}
+                            hiddenFields={['id', 'account_type']}
+                            onRowClick={(e, data) => {
+                              const id = (data as { id: string })?.id
+                              router.push({
+                                pathname: '/cash-transfer/[id]',
+                                query: {
+                                  id,
+                                },
+                              })
+                            }}
+                            hidePagination
+                            tableCellProps={{
+                              name: {
+                                style: {
+                                  minWidth: '30%',
+                                },
+                              },
+                              bank_accounts: {
+                                style: {
+                                  width: '30%',
+                                },
+                              },
+                              bank_balances: {
+                                style: {
+                                  width: '20%',
+                                },
+                              },
+                              balance: {
+                                style: {
+                                  width: '20%',
+                                },
+                              },
+                            }}
+                            renderCell={{
+                              bank_accounts: (value) => (
+                                <Box display="flex" flexWrap="wrap">
+                                  {value && Array.isArray(value)
+                                    ? (value as CaesarBank[])
+                                        ?.sort((ea1, ea2) =>
+                                          ea1.bank.name?.localeCompare(ea2.bank.name)
+                                        )
+                                        .map((ea) => (
+                                          <RoleBadge
+                                            style={{
+                                              marginTop: 4,
+                                              marginRight: 4,
+                                              borderRadius: '4em',
+                                              paddingLeft: 16,
+                                              paddingRight: 16,
+                                            }}
+                                            disablePopUp
+                                          >
+                                            {ea.bank.name}
+                                          </RoleBadge>
+                                        ))
+                                    : null}
+                                </Box>
+                              ),
+                            }}
+                          />
+                        </Box>
+                      ))}
+                  </Box>
+
+                  <TablePagination
+                    rowsPerPageOptions={[50, 100, 250, 500]}
+                    count={ceasarMetaData?.total || 0}
+                    rowsPerPage={query?.limit || 0}
+                    page={query?.page || 0}
+                    onPageChange={(_, page) => {
+                      // setPage(page)
+                      setQueryState('page')(page)
+                    }}
+                    onRowsPerPageChange={(
+                      e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+                    ) => {
+                      setQueryState('limit')(Number(e.target.value))
+                    }}
+                    component="div"
+                  />
+                </>
+              )
+            )}
+          </Box>
+        </Box>
+      </Paper>
+    </>
+  )
+}
